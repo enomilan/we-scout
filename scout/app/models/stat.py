ce5a38ea6a4a814a9cb776ba0a4ff0675ee81273
enomilan <andrewmusta@gmail.com>
2021-06-28 21:15:08 -0500

    added db

--- scout/app/models/stat.py
+++ scout/app/models/stat.py
@@ -1,22 +1,22 @@
 from .db import db
 
-class Comment(db.Model):
-    __tablename__='comments'
+class Stat(db.Model):
+    __tablename__='stats'
 
     id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     player_id = db.Column(db.Integer, db.ForeignKey('players.id'), nullable=False)
-    comment = db.Column(db.Text)
-    rating = db.Column(db.Integer)
+    games = db.Column(db.Text)
+    goals = db.Column(db.Integer)
+    xga = db.Column(db.Integer)
 
-    user = db.relationship('User')
     player = db.relationship('Player')
 
     def to_dict(self):
         return {
             "id": self.id,
-            "user_id": self.user_id,
             "player_id": self.player_id,
-            "comment": self.comment,
+            "games": self.games,
+            "goals": self.goals,
+             "xga" : self.xga
       
     }
