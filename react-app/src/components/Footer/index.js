470fbe11790a3c59f570e3693d1efaa7503c7df3
enomilan <andrewmusta@gmail.com>
2021-10-04 13:13:59 -0500

    footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -4,7 +4,13 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <h1>this is a test</h1>
+            <h1>Andrew Musta</h1>
+            <a href="https://www.linkedin.com/in/andrew-musta-a46506217/" rel="noopener noreferrer" target="_blank">
+                Connect  |
+            </a>
+            <a href="https://github.com/enomilan/we-scout" rel="noopener noreferrer" target="_blank">
+                Github  |
+            </a>
         </div>
     )
 }
