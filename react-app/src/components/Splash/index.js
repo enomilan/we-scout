845eed05c580ca2df47e66b206b6a8ca06616e3f
enomilan <andrewmusta@gmail.com>
2021-10-18 12:10:27 -0500

    welcome page

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,13 +1,16 @@
 import React from "react";
+import pic1 from '../../images/scouting.png'
 
 const Splash = () => {
 
 
     return (
-        
+
         <div className='container'>
             <div className='Main'>
-                <h1>Welcome to WeScout</h1>
+                <div id='top'>WeScout is a website where you can watch and scout players in the world of Association Football(soccer).</div>
+                <img id='pic1' src={pic1}></img>
+                <div id='bottom'>Scout players from the top leagues, Premier League, Serie A, La Liga, Bundesliga, Ligue 1 and more.</div>
             </div>
 
         </div>
