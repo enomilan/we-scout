700aafb2786990e084efadc577950018079ab3b7
enomilan <andrewmusta@gmail.com>
2021-11-02 19:57:44 -0500

    fixed button

--- react-app/src/components/Comment/commentbody.js
+++ react-app/src/components/Comment/commentbody.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import * as commentReducer from '../../store/comment'
-
+import { Button } from '@material-ui/core'
 
 export default function Commentbody({comment}) {
 
@@ -58,8 +58,8 @@ export default function Commentbody({comment}) {
             
             {user && comment.user_id === user.id && 
                 <>
-                <button onClick={editIt}id='comments_button'>Edit</button>
-                <button onClick={deleted}id='comments_button'>Delete</button> </> } </> )}
+                <Button onClick={editIt}id='comments_button'>Edit</Button>
+                <Button onClick={deleted}id='comments_button'>Delete</Button> </> } </> )}
         </div> 
     )
 }
