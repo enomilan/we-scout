375466cb04ebc5210870d9892dfa3c5c05ac15ea
enomilan <andrewmusta@gmail.com>
2021-11-02 19:55:41 -0500

    fixed button

--- react-app/src/components/Comment/index.js
+++ react-app/src/components/Comment/index.js
@@ -70,7 +70,7 @@ const Comment = () => {
                         Comment
                     </textarea>
 
-                    <button type='submit' id='comment_button'>Post</button>
+                    <Button type='submit' id='comment_button'>Post</Button>
 
                 </form>
             </div>
